3060ae0d712d4c5980fe3f70c15377cea740246d
Kninja99 <kcdamschen@yahoo.com>
2023-08-12 10:47:33 -0700

    basic layout of landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="main" class="bg-dark-gray h-screen w-screen overflow-hidden">
     <Header />
+    <Landing />
   </div>
 </template>
 
 <script>
 // import header component
 import Header from './components/Header.vue';
+import Landing from './components/Landing.vue';
 export default {
   name: 'App',
   components: {
     Header,
+    Landing,
   },
 };
 </script>
---
+++ frontend/src/components/Landing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="text-light-blue">Hi, my name is</p>
+    <h1 class="text-white">Kyle Damschen</h1>
+    <!-- <h1>I build things through technology.</h1> -->
+    <p class="text-dark-white">
+      I'm a career motivated software engineer that is currently working on a
+      variety of projects with the Citrus Clonal Protection Program to increase
+      productivity of lab teams through technology. I am currently seeking to
+      enhance my technical knowledge and solve complex problems as a software
+      engineer.
+    </p>
+    <button class="text-white bg-btn-blue hover:bg-dark-blue p-2 rounded-2xl">
+      Resume
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+};
+</script>
