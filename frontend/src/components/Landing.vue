3946c4c63f3314fdcc84e662fa121fc870d5b268
Kninja99 <kcdamschen@yahoo.com>
2023-08-13 09:36:29 -0700

    text sizing change

--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -1,10 +1,14 @@
 <template class="w-screen">
   <div class="mt-32 mx-12 cursor-default flex flex-col items-center">
     <div class="max-w-3xl">
-      <p class="text-light-blue text-xl">Hi, my name is</p>
-      <h1 class="text-white text-4xl my-5">Kyle Damschen.</h1>
+      <p class="text-light-blue text-xl md:text-3xl lg:text-4xl">
+        Hi, my name is
+      </p>
+      <h1 class="text-white text-4xl my-5 md:text-5xl lg:text-6xl">
+        Kyle Damschen.
+      </h1>
       <!-- <h1>I build things through technology.</h1> -->
-      <p class="text-dark-white text-base">
+      <p class="text-dark-white text-base lg:text-lg">
         I'm a career motivated software engineer that is currently working on a
         variety of projects with the Citrus Clonal Protection Program to
         increase productivity of lab teams through technology. I am currently
