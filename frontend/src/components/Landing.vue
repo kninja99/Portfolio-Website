80727832762534deb0f5233cc32af31282a959e9
Kninja99 <kcdamschen@yahoo.com>
2023-08-13 09:30:31 -0700

    landing centered and reactive

--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -1,20 +1,22 @@
-<template>
-  <div class="mt-32 mx-12">
-    <p class="text-light-blue text-xl">Hi, my name is</p>
-    <h1 class="text-white text-4xl my-5">Kyle Damschen.</h1>
-    <!-- <h1>I build things through technology.</h1> -->
-    <p class="text-dark-white text-base">
-      I'm a career motivated software engineer that is currently working on a
-      variety of projects with the Citrus Clonal Protection Program to increase
-      productivity of lab teams through technology. I am currently seeking to
-      enhance my technical knowledge and solve complex problems as a software
-      engineer.
-    </p>
-    <button
-      class="text-light-blue border-solid border-2 border-light-blue p-2 rounded-2xl w-32 my-10"
-    >
-      Resume
-    </button>
+<template class="w-screen">
+  <div class="mt-32 mx-12 cursor-default flex flex-col items-center">
+    <div class="max-w-3xl">
+      <p class="text-light-blue text-xl">Hi, my name is</p>
+      <h1 class="text-white text-4xl my-5">Kyle Damschen.</h1>
+      <!-- <h1>I build things through technology.</h1> -->
+      <p class="text-dark-white text-base">
+        I'm a career motivated software engineer that is currently working on a
+        variety of projects with the Citrus Clonal Protection Program to
+        increase productivity of lab teams through technology. I am currently
+        seeking to enhance my technical knowledge and solve complex problems as
+        a software engineer.
+      </p>
+      <button
+        class="text-light-blue border-solid border-2 border-light-blue p-2 rounded-2xl w-32 my-10"
+      >
+        Resume
+      </button>
+    </div>
   </div>
 </template>
 
