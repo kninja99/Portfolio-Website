b2f8101c25e096309760a247b541dc4401f01262
Kninja99 <kcdamschen@yahoo.com>
2023-08-14 17:18:11 -0700

    basic structure of about page

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hello! I'm Kyle Damschen, an enthusiastic recent graduate with a
+        Computer Science degree, eager to embark on a journey in the world of
+        software engineering. My passion for technology and problem-solving has
+        driven me to pursue a career where I can transform innovative ideas into
+        functional, user-centric software solutions.
+      </p>
+      <p>
+        Currently I am working with the Citrus Clonal Protection Program at UC
+        Riverside, where I am taking advantage of technology to enhance lab
+        teams performance and their experiance. I have also worked with ACM@UCR
+        as a software developer, where I have been able to work on a variety of
+        projects and gain valuable experience.
+      </p>
+      <p>
+        When I'm not coding, I enjoy playing video games, working out, and
+        spending time with my friends and family.
+      </p>
+      <p>Here are a few technologies I've been working with recently:</p>
+      <ul>
+        <li>JavaScript (ES6+)</li>
+        <li>Node.js</li>
+        <li>Python</li>
+        <li>C++</li>
+        <li>HTML & CSS</li>
+        <li>Git</li>
+        <li>SQL</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
