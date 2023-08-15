17ac20d10f27f883413107a4b560f8d1e33f3b37
Kninja99 <kcdamschen@yahoo.com>
2023-08-14 18:10:34 -0700

    basic styling for about me

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -1,41 +1,54 @@
-<template>
-  <div>
-    <div>
-      <h1>About Me</h1>
-      <p>
+<template class="w-screen">
+  <div
+    class="text-dark-white flex flex-col mt-36 mx-12 items-center justify-center"
+  >
+    <div class="max-w-4xl">
+      <SectionHeader title="About Me" />
+      <p class="mb-3">
         Hello! I'm Kyle Damschen, an enthusiastic recent graduate with a
         Computer Science degree, eager to embark on a journey in the world of
         software engineering. My passion for technology and problem-solving has
         driven me to pursue a career where I can transform innovative ideas into
         functional, user-centric software solutions.
       </p>
-      <p>
+      <p class="mb-3">
         Currently I am working with the Citrus Clonal Protection Program at UC
         Riverside, where I am taking advantage of technology to enhance lab
         teams performance and their experiance. I have also worked with ACM@UCR
         as a software developer, where I have been able to work on a variety of
         projects and gain valuable experience.
       </p>
-      <p>
-        When I'm not coding, I enjoy playing video games, working out, and
-        spending time with my friends and family.
+      <p class="mb-3">
+        Here are a few technologies I've been working with recently:
       </p>
-      <p>Here are a few technologies I've been working with recently:</p>
-      <ul>
-        <li>JavaScript (ES6+)</li>
-        <li>Node.js</li>
-        <li>Python</li>
-        <li>C++</li>
-        <li>HTML & CSS</li>
-        <li>Git</li>
-        <li>SQL</li>
-      </ul>
+      <div class="flex sm:w-3/6 sm:justify-evenly">
+        <ul class="mx-1 my-2">
+          <li>JavaScript (ES6+)</li>
+          <li>Node.js</li>
+          <li>Python</li>
+          <li>C++</li>
+        </ul>
+        <ul class="mx-1 my-2">
+          <li>HTML & CSS</li>
+          <li>Git</li>
+          <li>SQL</li>
+        </ul>
+      </div>
     </div>
+    <img
+      class="w-64"
+      src="../assets/imgs/Personal_Photo.jpg"
+      alt="personal Photo"
+    />
   </div>
 </template>
 
 <script>
+import SectionHeader from './SectionHeader.vue';
 export default {
   name: 'About',
+  components: {
+    SectionHeader,
+  },
 };
 </script>
---
+++ frontend/src/components/SectionHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1
+    class="mx-0 mt-[10px] mb-[20px] border-b-[1px] border-light-blue leading-[1em]"
+  >
+    <span
+      class="text-2xl text-white bg-dark-gray pr-5 py-[0px] top-[14px] relative"
+      >{{ title }}</span
+    >
+  </h1>
+  <template />
+</template>
+
+<script>
+export default {
+  name: 'AboutMe',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
