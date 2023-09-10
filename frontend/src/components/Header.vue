99fb2368892c1e89a07fe81964ac79c902eb76d0
Kninja99 <kcdamschen@yahoo.com>
2023-09-10 11:16:46 -0700

    Nav bar comp added

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,23 +4,17 @@
     <!-- website logo (kyle_logo) -->
     <img src="../assets/imgs/Kyle_Logo.svg" alt="kyle_logo" />
     <!-- quick nav btns for single page applications -->
-    <div class="flex">
-      <NavBtn btnText="About" btnLink="/" />
-      <NavBtn btnText="Work" btnLink="/work" />
-      <NavBtn btnText="Projects" btnLink="/projects" />
-      <NavBtn btnText="Contact" btnLink="/contact" />
-    </div>
+    <NavBar />
   </div>
 </template>
 
 <!-- export  -->
 <script>
-// import nav btn component
-import NavBtn from './NavBtn.vue';
+import NavBar from './NavBar.vue';
 export default {
   name: 'Header',
   components: {
-    NavBtn,
+    NavBar,
   },
 };
 </script>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <NavBtn btnText="About" btnLink="/" />
+    <NavBtn btnText="Work" btnLink="/work" />
+    <NavBtn btnText="Projects" btnLink="/projects" />
+    <NavBtn btnText="Contact" btnLink="/contact" />
+  </nav>
+</template>
+
+<script>
+import NavBtn from './NavBtn.vue';
+export default {
+  name: 'NavBar',
+  components: {
+    NavBtn,
+  },
+};
+</script>
