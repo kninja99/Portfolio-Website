e6f7553acf01f2ed6580c0d295e23eac46142a07
Kninja99 <kcdamschen@yahoo.com>
2023-09-10 12:24:39 -0700

    toggling of nav bar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -5,17 +5,27 @@ export default {
   components: {
     NavBtn,
   },
+  methods: {
+    // toggle mobile nav
+    toggleMobileNav() {
+      const nav = document.querySelector('nav');
+      nav.classList.toggle('hidden');
+    },
+  },
 };
 </script>
 
 <template>
   <div class="flex flex-col">
     <!-- mobile nav btn -->
-    <button class="h-8 w-8 self-end fixed top-7 z-10 md:hidden">
+    <button
+      class="h-8 w-8 self-end fixed top-7 z-10 md:hidden"
+      @click="toggleMobileNav"
+    >
       <img src="../assets/imgs/mobile-nav-btn.svg" alt="navBtn" />
     </button>
     <nav
-      class="absolute bg-project-black/70 right-0 top-0 py-14 h-[100%] w-[40%] flex flex-col bg:project-black md:flex-row md:relative md:top-0 md:right-0 md:bg-dark-gray md:h-auto md:w-auto md:py-0"
+      class="absolute bg-project-black/70 right-0 top-0 py-14 h-[100%] w-[40%] flex hidden flex-col bg:project-black md:flex md:flex-row md:relative md:top-0 md:right-0 md:bg-dark-gray md:h-auto md:w-auto md:py-0"
     >
       <NavBtn btnText="About" btnLink="/" />
       <NavBtn btnText="Work" btnLink="/work" />
