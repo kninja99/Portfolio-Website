422e6b7763c22c5f7183fb978695e9c574630704
Kninja99 <kcdamschen@yahoo.com>
2023-09-10 12:27:11 -0700

    width adjustment

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -25,7 +25,7 @@ export default {
       <img src="../assets/imgs/mobile-nav-btn.svg" alt="navBtn" />
     </button>
     <nav
-      class="absolute bg-project-black/70 right-0 top-0 py-14 h-[100%] w-[40%] flex hidden flex-col bg:project-black md:flex md:flex-row md:relative md:top-0 md:right-0 md:bg-dark-gray md:h-auto md:w-auto md:py-0"
+      class="absolute bg-project-black/70 right-0 top-0 py-14 h-[100%] w-[60%] sm:w-[40%] flex hidden flex-col bg:project-black md:flex md:flex-row md:relative md:top-0 md:right-0 md:bg-dark-gray md:h-auto md:w-auto md:py-0"
     >
       <NavBtn btnText="About" btnLink="/" />
       <NavBtn btnText="Work" btnLink="/work" />
