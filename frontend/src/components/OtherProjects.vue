7328816bf0328ad4ce6024b67bb9e49832f4c817
Kninja99 <kcdamschen@yahoo.com>
2023-09-10 19:15:48 -0700

    view archive hover effect

--- frontend/src/components/OtherProjects.vue
+++ frontend/src/components/OtherProjects.vue
@@ -4,11 +4,15 @@
     <div class="flex flex-col justify-center items-center">
       <h3 class="text-white text-2xl md:text-3xl">Other Noteworthy Projects</h3>
       <a
-        class="text-light-blue mt-5"
         href="https://github.com/kninja99"
         target="_blank"
-        >View the Archive</a
+        class="relative text-light-blue mt-5 group"
       >
+        <span
+          class="absolute inset-x-0 bottom-0 h-0.5 bg-light-blue transform scale-x-0 group-hover:scale-x-100 transition-transform origin-middle"
+        ></span>
+        View the Archive
+      </a>
     </div>
     <!-- Projects -->
     <div class="flex flex-wrap mt-8 justify-center items-center">
