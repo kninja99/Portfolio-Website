113401ff8234ce2681d99ac8f78a1998e3c4f61b
Kninja99 <kcdamschen@yahoo.com>
2023-09-10 17:05:18 -0700

    project link
    * added project link
    * softed white value

--- frontend/src/components/ProjectCard.vue
+++ frontend/src/components/ProjectCard.vue
@@ -1,30 +1,32 @@
 <template>
-  <div
-    class="w-[300px] h-[300px] bg-project-black/90 rounded-md shadow-[6px_6px_10px_#000] flex flex-col justify-between mx-4 my-6"
-  >
-    <!-- header for icon -->
-    <div class="py-5 mx-5">
-      <img
-        class="w-[40px] h-[40px]"
-        src="../assets/imgs/Folder-Icon.svg"
-        alt="folder"
-      />
+  <a href="https://github.com/kninja99" target="_blank">
+    <div
+      class="w-[300px] h-[300px] bg-project-black/90 rounded-md shadow-[6px_6px_10px_#000] flex flex-col justify-between mx-4 my-6"
+    >
+      <!-- header for icon -->
+      <div class="py-5 mx-5">
+        <img
+          class="w-[40px] h-[40px]"
+          src="../assets/imgs/Folder-Icon.svg"
+          alt="folder"
+        />
+      </div>
+      <!-- Project Details -->
+      <div class="text-white mx-5">
+        <p class="text-xl">Project Title</p>
+        <p class="my-2 text-dark-white/80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          dolores voluptates quidem non commodi error.
+        </p>
+      </div>
+      <ul class="list-none flex flex-wrap text-dark-white/70 text-sm my-2 mx-2">
+        <!-- create a li for each skill in skills prop -->
+        <li class="m-2">Skill 1</li>
+        <li class="m-2">Skill 2</li>
+        <li class="m-2">Skill 3</li>
+      </ul>
     </div>
-    <!-- Project Details -->
-    <div class="text-white mx-5">
-      <p class="text-xl">Project Title</p>
-      <p class="my-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        dolores voluptates quidem non commodi error.
-      </p>
-    </div>
-    <ul class="list-none flex flex-wrap text-white text-sm my-2 mx-2">
-      <!-- create a li for each skill in skills prop -->
-      <li class="m-2">Skill 1</li>
-      <li class="m-2">Skill 2</li>
-      <li class="m-2">Skill 3</li>
-    </ul>
-  </div>
+  </a>
 </template>
 
 <script>
