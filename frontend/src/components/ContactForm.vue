8f89a802bde04cc3e9496605d8e52c33efba1951
Kninja99 <kcdamschen@yahoo.com>
2023-09-11 10:01:56 -0700

    form init styling
    * for is now styled with theme
    * need to now implement form validation before we send email

--- frontend/src/components/ContactForm.vue
+++ frontend/src/components/ContactForm.vue
@@ -13,14 +13,37 @@
         </p>
       </div>
       <div
-        class="bg-project-black/90 rounded-md shadow-[6px_6px_10px_#000] p-10"
+        class="bg-project-black/90 rounded-md shadow-[6px_6px_10px_#000] px-5 py-5 flex flex-col items-center"
       >
-        <form class="flex flex-col" action="">
-          <input name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="youremail@domain.com" />
-          <textarea name="message" placeholder=""></textarea>
+        <form class="flex flex-col items-center w-[100%]" action="">
+          <p
+            class="text-light-blue text-2xl mb-5 underline underline-offset-4 hover:cursor-default"
+          >
+            Contact Me
+          </p>
+          <input
+            class="bg-dark-gray text-dark-white px-2 py-1 m-2 border-solid border-2 border-light-teal rounded-2xl w-[300px]"
+            name="name"
+            placeholder="Name"
+          />
+          <input
+            class="bg-dark-gray text-dark-white px-2 py-1 m-2 border-solid border-2 border-light-teal rounded-2xl w-[300px]"
+            type="email"
+            name="email"
+            placeholder="youremail@domain.com"
+          />
+          <textarea
+            class="bg-dark-gray text-dark-white px-3 py-2 m-2 border-solid border-2 border-light-teal rounded-2xl w-[100%] h-[200px]"
+            name="message"
+            placeholder="Type message here..."
+          ></textarea>
           <!-- your other form fields go here -->
-          <button type="submit">Send</button>
+          <button
+            class="text-light-blue border-solid border-2 border-light-blue p-2 rounded-2xl w-32 mt-4 hover:animate-shadowPop"
+            type="submit"
+          >
+            Send
+          </button>
         </form>
       </div>
     </div>
