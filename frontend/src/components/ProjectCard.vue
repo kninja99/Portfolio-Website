cd350e11aed0ca2137bb4c7ea7538a9927815e54
Kninja99 <kcdamschen@yahoo.com>
2023-09-10 18:54:17 -0700

    card skills allignment fix

--- frontend/src/components/ProjectCard.vue
+++ frontend/src/components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <a :href="link" target="_blank" @click="cardClickEvent">
     <div
-      class="w-[300px] h-[300px] bg-project-black/90 rounded-md shadow-[6px_6px_10px_#000] flex flex-col justify-between mx-4 my-6 flex-1 basis-1/3 hover:animate-cardHoverUp animate-cardHoverDown"
+      class="w-[300px] h-[300px] bg-project-black/90 rounded-md shadow-[6px_6px_10px_#000] flex flex-col mx-4 my-6 flex-1 basis-1/3 hover:animate-cardHoverUp animate-cardHoverDown"
     >
       <!-- header for icon -->
       <div class="py-5 mx-5">
@@ -18,7 +18,9 @@
           {{ description }}
         </p>
       </div>
-      <ul class="list-none flex flex-wrap text-dark-white/70 text-sm my-2 mx-2">
+      <ul
+        class="list-none flex flex-wrap text-dark-white/70 text-sm my-2 mx-2 mt-auto"
+      >
         <!-- create a li for each skill in skills prop -->
         <li class="m-2" v-for="(skill, key) in skills" :key="key">
           {{ skill }}
