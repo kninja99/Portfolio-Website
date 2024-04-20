7e28ff221a82f3d00589d4b64d14bb1ecc0032b8
kninja99 <kcdamschen@gmail.com>
2024-04-20 16:08:13 -0700

    delpoy script fixed for linux

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env sh
-
 # abort on errors
 set -e
 
@@ -10,7 +8,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.kyledamschen.com' > CNAME
 
 git init
 git add -A
@@ -22,5 +20,3 @@ git remote add origin https://github.com/kninja99/kninja99.github.io.git
 git push -u -f origin master
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-cd -
-pause
